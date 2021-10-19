363ce05285d8be59eca986a70a873fdbe2c91ac1
Pavel Fedorov <87358389+bakaYaR0@users.noreply.github.com>
2021-10-19 16:40:05 +0300

    Update BilliardsTask.cs

--- BilliardsTask.cs
+++ BilliardsTask.cs
@@ -4,15 +4,8 @@ namespace Billiards
 {
     public static class BilliardsTask
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="directionRadians">Угол направления движения шара</param>
-        /// <param name="wallInclinationRadians">Угол</param>
-        /// <returns></returns>
         public static double BounceWall(double directionRadians, double wallInclinationRadians)
         {
-            //TODO
             return (wallInclinationRadians - directionRadians) + wallInclinationRadians;
         }
     }

828cfd233b22755a055e9ec35749956c52549e98
Pavel Fedorov <87358389+bakaYaR0@users.noreply.github.com>
2021-10-19 16:45:39 +0300

    Add files via upload

---
+++ Percentages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Percentages
+{
+    class Percentages
+    {
+        public static double Calculate(string userInput)
+        {
+            var substrings = userInput.Split();
+            var amount = Double.Parse(substrings[0]);
+            var percentage = Double.Parse(substrings[1]);
+            var monthsPassed = Double.Parse(substrings[2]);
+
+            double sum = amount * (Math.Pow((1 + (percentage / 100) / 12), monthsPassed));
+            return sum;
+        }
+    }
+}
